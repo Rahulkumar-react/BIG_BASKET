e85830e9cfd5c3874fe5934568604d873ca431b3
RaviKGR <ravimuthukgr@gmail.com>
2024-06-01 11:23:14 +0530

    align Navbar

--- src/Pages/LandingScreen.js
+++ src/Pages/LandingScreen.js
@@ -1,17 +1,19 @@
-import React from 'react';
-import Navbar from './Nanbar/index';
-import Sidebar from '../Components/Sidebar';
+import React from "react";
+import Navbar from "./Nanbar/index";
+import Sidebar from "../Components/Sidebar";
+import { Container } from "@mui/material";
 // import Sidebar from '../Components/Sidebar';
 // import Sidebar from '../Components/Sidebar';
 
 const LandingScreen = () => {
-
   return (
     <div>
-     <Navbar/>
-     <Sidebar />
+      {/* <Container> */}
+        <Navbar />
+        <Sidebar />
+      {/* </Container> */}
     </div>
-  )
-}
+  );
+};
 
-export default LandingScreen
+export default LandingScreen;
