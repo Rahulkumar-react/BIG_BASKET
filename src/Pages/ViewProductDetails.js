66146c60de6b6e29788024226266bb608802e565
RaviKGR <ravimuthukgr@gmail.com>
2024-06-05 11:08:35 +0530

    navbar and side bar change

---
+++ src/Pages/ViewProductDetails.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import ProductDetails from '../Components/ProductDetails'
+import Navbar from './Nanbar'
+
+const ViewProductDetails = () => {
+  return (
+    <>
+    <Navbar />
+    <ProductDetails />
+    </>
+  )
+}
+
+export default ViewProductDetails
